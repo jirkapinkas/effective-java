cea605690f92d326b2ac5adb1f921fd471e78e63
JPINKAS <j_pinkas@kb.cz>
2022-10-04 09:38:46 +0200

    Static factory methods instead of constructors

--- src/main/java/com/test/effectivejava/Main.java
+++ src/main/java/com/test/effectivejava/Main.java
@@ -1,8 +1,16 @@
 package com.test.effectivejava;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        // Static factory methods instead of constructors
+        Boolean value1 = Boolean.valueOf("true"); // ANO
+        Boolean value2 = new Boolean("true"); // NEE
+
+        List<Integer> integers1 = List.of(1, 2, 3); // ANO
+        List<Integer> integers2 = Collections.unmodifiableList(new ArrayList<>(Arrays.asList(1,2,3))); // NEE
 
     }
 
